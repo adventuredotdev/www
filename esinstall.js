313ee69d5d1760561389349fabd4a66ec51f04a6
Christopher Biscardi <chris@christopherbiscardi.com>
2022-01-13 11:57:27 -0800

    init

---
+++ esinstall.js
@@ -0,0 +1,15 @@
+export const specs = [
+  "preact",
+  "preact/hooks",
+  "@mdx-js/preact",
+  "react-helmet",
+  "preact/compat",
+  "preact/jsx-runtime",
+];
+
+export const options = {
+  alias: {
+    react: "preact/compat",
+    "react/jsx-runtime": "preact/jsx-runtime",
+  },
+};
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import { h } from "preact";
+
+export default (props) => (
+  <div>
+    <div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8 lg:flex lg:justify-between">
+      <div class="max-w-xl text-center">
+        <h2 class="text-4xl font-extrabold text-white sm:text-5xl sm:tracking-tight lg:text-6xl">
+          Adventure.dev
+        </h2>
+        <p class="mt-5 text-xl text-white">Programming is an adventure.</p>
+      </div>
+    </div>
+  </div>
+);
